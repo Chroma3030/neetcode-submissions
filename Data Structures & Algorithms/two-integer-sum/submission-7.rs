3adc64125c82b647625aadfbc74ec0af9af6a841
Xisco Kid <95986312+Chroma3030@users.noreply.github.com>
2026-09-22 09:40:45 -0500

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.rs
@@ -0,0 +1,29 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut pairs: Vec<(i32, i32)> = nums
+        .iter()
+        .enumerate()
+        .map(|(i, &n)| (n, i as i32))
+        .collect();
+
+        pairs.sort_unstable_by_key(|&(n, _)| n);
+
+        let mut l = 0;
+        let mut r = nums.len() - 1;
+
+        while l < r {
+            let attempt = pairs[l].0 as i64 + pairs[r].0 as i64;
+
+            match attempt.cmp(&(target as i64)) {
+                Ordering::Equal => {
+                    let a = pairs[l].1;
+                    let b = pairs[r].1;
+                    return vec![pairs[l].1.min(b), pairs[r].1.max(a)];
+                },
+                Ordering::Greater => r -= 1,
+                Ordering::Less => l += 1,
+            }
+        }
+        unreachable!()
+    }
+}
